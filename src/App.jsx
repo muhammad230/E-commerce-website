6df63eaa39627a8947c6bf1c81a6197d98cc4784
muhammad230 <ali.khansmr3@gmail.com>
2026-04-01 21:55:48 +0500

    add new branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
+import Category from './Components/Category'
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero/>
+      <Category/>
     </div>
   )
 }
---
+++ src/Components/Category.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Category = () => {
+  return (
+    <section className="bg-white">
+      <div>
+        <h2 className="font-bold text-2xl">
+          <span className="bg-gradient-to-r from-purple-600 to-pink-400 bg-clip-text text-transparent">
+            Browse 
+          </span>
+          <span className="bg-gradient-to-r from-purple-600 to-pink-400 bg-clip-text text-transparent">
+            by
+          </span>
+          <span className="bg-gradient-to-r from-pink-400 to-purple-600 bg-clip-text text-transparent">
+            Category
+          </span>
+        </h2>
+      </div>
+    </section>
+  );
+};
+
+export default Category;
