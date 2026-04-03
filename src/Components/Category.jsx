c5866db0e9b883de6ca3f8f15f9f11c2a8316752
muhammad230 <ali.khansmr3@gmail.com>
2026-04-03 22:14:44 +0500

    change color of h2 and fixed

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -14,11 +14,14 @@ const categories = [
 const Category = () => {
      const [active, setActive] = useState("All");
   return (
-    <section className="px-6 py-10 bg-gray-100">
+    <section className="px-22 py-12 bg-gray-100">
       {/* Heading */}
-      <h2 className="text-2xl font-bold text-purple-600 mb-6 flex items-center gap-2">
-        📈 Browse by Category
-      </h2>
+    <h2 className="text-3xl font-bold mb-6 flex items-center gap-2">
+  📈 
+  <span className="bg-gradient-to-r from-purple-500 via-pink-500 to-purple-500 bg-clip-text text-transparent">
+    Browse by Category
+  </span>
+</h2>
 
       {/* Buttons */}
       <div className="flex flex-wrap gap-4">
