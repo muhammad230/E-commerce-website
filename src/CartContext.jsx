ddf047ec4c307159f2f73cb893b74899132ff1d0
muhammad230 <ali.khansmr3@gmail.com>
2026-04-13 13:50:24 +0500

    add function on clear cart

--- src/CartContext.jsx
+++ src/CartContext.jsx
@@ -13,8 +13,12 @@ export const CartProvider = ({ children }) => {
     setCart((prev) => prev.filter((item) => item.id !== id));
   };
 
+  const clearCart = () => {
+    setCart([]);
+  };
+
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart }}>
       {children}
     </CartContext.Provider>
   );
