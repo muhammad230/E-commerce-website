2f410406c304d8aebd09eb102f53b9fc7aa01cf4
muhammad230 <ali.khansmr3@gmail.com>
2026-04-14 14:44:02 +0500

    add new file

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Hero from './Components/Hero'
 import Category from './Components/Category'
 import CartPage from './Components/CartPage'
 import ProductDetail from './Components/ProductDetail'  // ← ADD THIS
+import CheckoutPage from "./Components/CheckoutPage";
+
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Route path="/" element={<><Hero /><Category /></>} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/product/:id" element={<ProductDetail />} />  {/* ← ADD THIS */}
+          <Route path="/checkout" element={<CheckoutPage />} />
+
         </Routes>
       </BrowserRouter>
     </CartProvider>
